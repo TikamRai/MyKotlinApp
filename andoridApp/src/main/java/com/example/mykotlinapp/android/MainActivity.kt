82b79716f22b7ff3378ea4ddb573496ad6bb3cd8
Tikam Rai <a00280792@mycambrian.ca>
2023-09-26 18:36:04 -0400

    Initial commit

---
+++ andoridApp/src/main/java/com/example/mykotlinapp/android/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.mykotlinapp.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mykotlinapp.Greeting
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    GreetingView(Greeting().greet())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingView(phrases: List<String>) {
+    LazyColumn(
+        contentPadding = PaddingValues(40.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+    ) {
+        items(phrases) { phrase ->
+            Text(phrase)
+            Divider()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView(listOf("Hello, Android!"))
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/mykotlinapp/Greeting.kt
@@ -0,0 +1,12 @@
+package com.example.mykotlinapp
+
+import kotlin.random.Random
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): List<String> = buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what it is! > ${platform.name.reversed()}!")
+    }
+}
