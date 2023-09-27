29f98a252fb3e25653c6fb23102e8dfd2eccde4b
Tikam Rai <a00280792@mycambrian.ca>
2023-09-27 18:50:55 -0400

    Test_03

--- shared/src/commonMain/kotlin/com/example/mykotlinapp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/mykotlinapp/Greeting.kt
@@ -1,5 +1,6 @@
 package com.example.mykotlinapp
 
+import daysUntilNewYear
 import kotlin.random.Random
 import kotlin.annotation.AnnotationRetention
 
@@ -9,5 +10,6 @@ class Greeting {
     fun greet(): List<String> = buildList {
         add(if (Random.nextBoolean()) "Hi!" else "Hello!")
         add("Guess what it is man! > ${platform.name.reversed()}!")
+        add("\nThere are only ${daysUntilNewYear()} days left until New Year!")
     }
 }
